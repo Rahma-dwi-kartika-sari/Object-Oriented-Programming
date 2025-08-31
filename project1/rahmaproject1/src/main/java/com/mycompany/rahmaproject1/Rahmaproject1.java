28b9ea0fa82d49a90ef329e1fe5a469c7fc5becb
Rahma-dwi-kartika-sari <rahmaa.krty@gmail.com>
2025-08-31 23:16:50 +0700

    Project 1_Praktikum PBO pertemuan1

---
+++ project1/rahmaproject1/src/main/java/com/mycompany/rahmaproject1/Rahmaproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rahmaproject1;
+
+/**
+ *
+ * @author user
+ */
+public class Rahmaproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Rahma");
+        System.out.println("NPM: 2410506008");
+        System.out.println("Alamat: Magelang");
+        System.out.println("No_Hp: 083111112222");
+    }
+}
